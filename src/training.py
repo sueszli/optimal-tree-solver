ed410a06b3430a1c62c4014beae4d3fe72dd4faa
sueszli <jabaryyahya@gmail.com>
2025-01-27 16:46:50 +0100

    up

--- src/training.py
+++ src/training.py
@@ -2,6 +2,11 @@ from typing import Dict, List, Optional
 from dataclasses import dataclass
 
 
+# 
+# training
+# 
+
+
 @dataclass
 class Example:
     features: Dict[str, int]  # feature name -> value
@@ -32,6 +37,7 @@ def findth(examples: List[Example], tree: Node, feature_assignment: Dict[str, st
     # get feature of node from assignment
     feature = feature_assignment[id(tree)]
 
+    # find largest valid threshold for left child
     threshold = binary_search(examples, tree, feature_assignment, feature, tree.left)
     
     # try right subtree first
@@ -39,12 +45,11 @@ def findth(examples: List[Example], tree: Node, feature_assignment: Dict[str, st
     right_assignment = findth(right_examples, tree.right, feature_assignment)
     if right_assignment is None:
         return None
-        
+
     # then try left subtree
     left_examples = [e for e in examples if e.features[feature] <= threshold]
     left_assignment = findth(left_examples, tree.left, feature_assignment)
-    if left_assignment is None:
-        return None
+    assert left_assignment is not None
     
     # combine assignments
     return {**{feature: threshold}, **left_assignment, **right_assignment}
@@ -76,12 +81,30 @@ def binary_search(examples: List[Example], tree: Node, feature_assignment: Dict[
     return best_threshold
 
 
+# 
+# eval
+# 
+
 if __name__ == "__main__":
+    # 
+    # dataset based on figure 2
+    # 
+
+    # examples = [
+    #     Example({"temp": 37, "rain": 1, "time": 10, "day": 3}, is_positive=False),
+    #     Example({"temp": 68, "rain": 0, "time": 60, "day": 2}, is_positive=True),
+    #     Example({"temp": 53, "rain": 1, "time": 60, "day": 4}, is_positive=False),
+    #     Example({"temp": 53, "rain": 0, "time": 15, "day": 2}, is_positive=False),
+    #     Example({"temp": 60, "rain": 0, "time": 60, "day": 5}, is_positive=True),
+    #     Example({"temp": 51, "rain": 0, "time": 40, "day": 3}, is_positive=True),
+    #     Example({"temp": 71, "rain": 0, "time": 35, "day": 5}, is_positive=True)
+    # ]
+
     examples = [
-        Example({'temp': 37, 'rain': 1}, is_positive=False),
         Example({'temp': 68, 'rain': 0}, is_positive=True),
-        Example({'temp': 53, 'rain': 1}, is_positive=False),
         Example({'temp': 60, 'rain': 0}, is_positive=True),
+        Example({'temp': 53, 'rain': 1}, is_positive=False),
+        Example({'temp': 37, 'rain': 1}, is_positive=False),
     ]
 
     tree = Node(
@@ -91,15 +114,35 @@ if __name__ == "__main__":
 
     feature_assignment = {id(tree): 'temp'}  # assign 'temp' to root node
     
+
     threshold_assignment = findth(examples, tree, feature_assignment)
-    
-    if threshold_assignment:
-        print("found valid threshold assignment:", threshold_assignment)
-        for e in examples:
-            classification = "positive" if e.features['temp'] > threshold_assignment['temp'] else "negative"
-            success: bool = classification == 'positive' if e.is_positive else classification == 'negative'
-            if not success:
-                print("error in classification")
-                print(e.features)
-    else:
+    if not threshold_assignment:
         print("no valid threshold assignment found")
+        exit()
+    
+    # 
+    # eval
+    # 
+
+    def init_tree(tree: Node, feature_assignment: Dict[int, str] = {}, threshold_assignment: Dict[str, int] = {}):
+        if tree.is_leaf:
+            return
+        tree.feature = feature_assignment[id(tree)]
+        tree.threshold = threshold_assignment[tree.feature]
+        init_tree(tree.left, threshold_assignment)
+        init_tree(tree.right, threshold_assignment)
+
+    init_tree(tree, feature_assignment, threshold_assignment)
+
+    for example in examples:
+        actual = example.is_positive
+        def get_prediction(tree: Node, example: Example):
+            if tree.is_leaf:
+                return tree.is_positive
+            if example.features[tree.feature] <= tree.threshold:
+                return get_prediction(tree.left, example)
+            return get_prediction(tree.right, example)
+        pred = get_prediction(tree, example)
+        assert actual == pred, f"actual: {actual}, pred: {pred} - {example.features}"
+
+    print("tree is valid")
